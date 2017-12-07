1acdd7636379a1d58c4cc6e7d944d471dc199b39
JohnMartin95 <john.martin1225@gmail.com>
2017-12-07 18:33:18 -0500

    hotfix99

--- src/main/resources/static/js/customer_js/account.js
+++ src/main/resources/static/js/customer_js/account.js
@@ -11,6 +11,10 @@ function getCurrentReservations(){
 	ajaxWrapperGET('/home/customer/getCurrentReservations/'+$("#accountNo").val(), "getCurrentReservationsResult");
 }
 
+function getAllReservations(){
+	ajaxWrapperGET('/home/customer/getAllReservations/'+$("#accountNo").val(), "getAllReservationsResult");
+}
+
 function getRecommendedFlights(){
 	var url = '/home/rep/recommendedFlights/' + $("#accountNo").val();
 	ajaxWrapperGET(url, "getRecommendedFlightsResult");
